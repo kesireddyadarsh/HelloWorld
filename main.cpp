fb2178971ce5b430d06d70e2efd5fad757ef72da
adarsh kesireddy <akesireddy@nevada.unr.edu>
2016-10-16 08:14:25 -0700

    Alright this works
    Testing

---
+++ main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  TestProject
+//
+//  Created by ak on 10/16/16.
+//  Copyright © 2016 ak. All rights reserved.
+//
+
+#include <iostream>
+
+
+using namespace std;
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    cout << "Hello, World!\n";
+    return 0;
+}
